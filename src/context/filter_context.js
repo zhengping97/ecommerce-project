da43800d0b0dcac8db4c521365994fa163857aee
hengzhengping <hengzhengping@gmail.com>
2022-05-20 19:53:40 +0800

    updated products page

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -11,14 +11,26 @@ import {
   CLEAR_FILTERS,
 } from '../actions'
 import { useProductsContext } from './products_context'
+import GridView from '../components/GridView'
 
-const initialState = {}
+const initialState = {
+  filtered_products: [],
+  all_products: [],
+  grid_view: true,
+}
 
 const FilterContext = React.createContext()
 
 export const FilterProvider = ({ children }) => {
+  const { products } = useProductsContext();
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    dispatch({ type: LOAD_PRODUCTS, payload: products })
+  }, [products])
+
   return (
-    <FilterContext.Provider value='filter context'>
+    <FilterContext.Provider value={{ ...state }}>
       {children}
     </FilterContext.Provider>
   )
