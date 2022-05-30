ae6116da0b217bf953b724c67e404188075d1027
hengzhengping <hengzhengping@gmail.com>
2022-05-30 15:46:10 +0800

    test complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ import { links } from '../utils/constants'
 import CartButtons from './CartButtons'
 import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
-import { Cart } from '../pages'
+//import { Cart } from '../pages'
 
 const Nav = () => {
   const { openSidebar } = useProductsContext();
--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import Product from './Product';
+//import Product from './Product';
+
 const PageHero = ({ title, product }) => {
   return (
     <Wrapper>
